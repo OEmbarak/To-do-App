3e408153be02468386a6a3ecdcc049c6b37310f4
Ossama Embarak <o.embarak@gmail.com>
2023-07-24 14:47:45 +0300

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by HCT  on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
